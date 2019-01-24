f368f9a679196a449c61805e89867583484bf1dd
wjfstruggle <2399012862@qq.com>
2019-01-24 18:21:02 +0800

    vue-music

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '../src/assets/reset/reset.css' // 重置样式
+import '../src/assets/reset/public.css' // 公共样式
+
 Vue.config.productionTip = false
 
 new Vue({
